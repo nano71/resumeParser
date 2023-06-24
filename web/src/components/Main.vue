1304fa676be5e67e0f15775bb4bd2c00694b1bde
binbin <2052089494@qq.com>
2023-06-24 23:49:18 +0800

    写界面

--- web/src/components/Main.vue
+++ web/src/components/Main.vue
@@ -1,12 +1,18 @@
 <template>
   <div id="main">
-    <div class="content">
+    <div class="content" v-if="false">
       <div class="topTitle">
         渐进式简历解析系统
       </div>
+<!--      未开始上传-->
+
       <div class="uploadContent">
+      <div v-if="false">
+        <div class="transformBox">
+          <div v-for="item in 80" :key="item" class="transformItems"></div>
+        </div>
         <div class="inputBox">
-          <input class="input" type="file"/>
+          <input class="input" type="file" accept=".doc,.docx,.pdf,.png,.jpg,.jpeg,.bmp,.txt"/>
         </div>
         <div class="iconBox">
           <img src="../assets/file.png" alt="">
@@ -15,11 +21,24 @@
           <div>点击 或 拖拽</div>
           <div>即可上传简历文件</div>
         </div>
+      </div >
+        <div class="uploading">
+          <div class="uploadingIcon"></div>
+          <div class="uploadingText">文件: {{fileName}} 上传中</div>
+          <div class="uploadingBar">
+            <div class="current" :style="{width:current*100+'%'}"></div>
+          </div>
+        </div>
       </div>
-      <div class="parserButton">
+
+
+
+        <div class="parserButton">
         <div class="button">开始解析</div>
+<!--      上传完成-->
       </div>
     </div>
+    <ResumeDetail/>
   </div>
 </template>
 
@@ -28,6 +47,8 @@ img {
   width: 100%;
   height: 100%;
 }
+
+
 
 #main {
   //width: 100vw;
@@ -49,7 +70,7 @@ img {
 
   .uploadContent {
     position: relative;
-    width: 40vw;
+    width: 800px;
     height: 360px;
     padding: 100px;
     border: 4px solid #000000;
@@ -57,13 +78,66 @@ img {
     display: flex;
     flex-direction: column;
     justify-content: center;
+    overflow: hidden;
+    //悬浮态
+    &:hover .transformBox{
+      opacity: 1;
+      animation: 2s loops infinite linear;
+    }
+    .transformBox {
+      opacity: 0;
+      transform: translate(-50%, -50%) rotate(135deg);
+      position: absolute;
+      top: 50%;
+      left: 50%;
+      width: 150%;
+      //height: 150%;
+
+      @keyframes loops {
+        0% {
+          transform: translate(-50%, -50%) rotate(135deg);
+        }
+
+        100% {
+          transform:translate(-39%, -39%) rotate(135deg);
+        }
+
+      }
+
+
+      .transformItems {
+        width: 100%;
+        height: 20px;
+        background-color: #ffffff;
+
+        &:nth-child(even) {
+          background-color: #000000;
+          opacity: .05;
+        }
+      }
+
+    }
+
     .inputBox {
+
       .input {
-        display: none;
+        cursor: pointer;
+
+        position: absolute;
+        z-index: 1;
+        width: 100%;
+        height: 100%;
+        left: 0;
+        top: 0;
+        opacity: 0;
+        //width: 100px;
+        //height: 100px;
+        //display: none;
       }
     }
 
     .iconBox {
+      position: relative;
       display: flex;
       justify-content: center;
       //margin-top: 80px;
@@ -75,6 +149,7 @@ img {
     }
 
     .textBox {
+      position: relative;
       //margin-top: 85px;
 
       :nth-child(1) {
@@ -91,22 +166,62 @@ img {
       }
     }
 
+    .uploading{
+      width: 100%;
+      height: 100%;
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+      @keyframes loops2{
+        0%{
+          transform: rotate(0deg);
+        }
+        100%{
+          transform: rotate(360deg);
+        }
+      }
+      .uploadingIcon{
+        width: 80px;
+        height: 80px;
+        border-radius:50% ;
+        border:4px solid #00000010;
+        border-top-color: black;
+        animation: 1s loops2 infinite linear;
+        margin-bottom: 20px;
+      }
+      .uploadingBar{
+        width: 100%;
+        height: 4px;
+        background-color: #00000010;
+        position: absolute;
+        bottom: 0;
+      .current{
+        height: inherit;
+        background-color: #000000;
+      }
+      }
+    }
   }
 
   .parserButton {
     margin-top: 80px;
-    .button{
+    display: flex;
+    justify-content: center;
+
+    .button {
       transition: .2s;
       font-size: 22px;
       font-weight: bold;
-      display: inline-block;
       padding: 10px 30px;
       background-color: #ffffff;
       border: 4px solid #000000;
-      &::selection{
+
+      &::selection {
         background: transparent;
       }
-      &:hover{
+
+      &:hover {
         user-select: none;
         cursor: pointer;
         background-color: #000000;
@@ -118,10 +233,25 @@ img {
 
 </style>
 <script>
+import ResumeDetail from "./resumeDetail.vue";
 export default {
+  components:{ResumeDetail},
   data() {
     return {
-      msg: "模板语法"
+      current:0.1,
+      msg: "模板语法",
+      fileName:"untitled.pdf",
+      personData:null
+    }
+  },
+  created() {
+    this.personData = {
+      name:"唐斌",
+      age:"22",
+      phone:"13481323106",
+      email:"2052089494@qq.com",
+      highestEducation:"本科",
+      school:"桂林理工大学"
     }
   }
 }
---
+++ web/src/components/resumeDetail.vue
@@ -0,0 +1,248 @@
+<template>
+  <div id="resumeDetail">
+    <div class="topBox">
+      <div class="personDetail">
+        <div class="headImg"></div>
+        <div class="detailCard">
+          <div class="top">
+            <span class="name">唐斌</span>
+            <span class="label">技术达标</span>
+          </div>
+          <div class="bottom">
+            <div class="item">
+              <div class="icon"></div>
+              <div class="text">22岁</div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="navBar">
+        <div class="navItem select">简历详情</div>
+        <div class="navItem">分析数据</div>
+      </div>
+    </div>
+    <div class="contentBox">
+      <div class="title">
+        <sapn>基本信息</sapn>
+        <div class="titleMask"></div>
+      </div>
+      <div class="messageDetails">
+        <div class="messageItem" v-for="item in 6" :key="item">
+          <div class="doll"></div>
+          <div class="items">
+            <span class="garyText">
+              姓名 :
+            </span>
+            <span>唐斌</span>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="contentBox">
+      <div class="title">
+        <sapn>教育背景</sapn>
+        <div class="titleMask"></div>
+      </div>
+      <div class="educationDetail">
+        <div class="schoolName">
+          <span class="name">桂林理工大学</span>
+          <span class="time garyText">2019.09 - 至今</span>
+        </div>
+        <div class="educationItem">
+          <div class="items" v-for="item in 6">
+            <span class="garyText">开始时间 : </span>
+            <span>2019.09</span>
+          </div>
+        </div>
+      </div>
+
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "resumeDetail"
+}
+</script>
+
+<style lang="less" scoped>
+#resumeDetail {
+  width: 800px;
+  min-height: 99vh;
+  border: 4px solid #000000;
+  display: flex;
+  flex-direction: column;
+  row-gap: 40px;
+  padding: 0 20px;
+}
+
+.garyText {
+  color: #00000080;
+}
+
+.topBox {
+  padding: 20px 20px 0 20px;
+  border-bottom: 1px solid #00000040;
+
+  .personDetail {
+    display: flex;
+
+    .headImg {
+      width: 60px;
+      height: 70px;
+      background-color: #000000;
+      margin-right: 20px;
+    }
+
+    .detailCard {
+      display: flex;
+      flex-direction: column;
+      justify-content: space-between;
+      padding-bottom: 10px;
+
+      .top {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        margin-bottom: 10px;
+
+        .name {
+          font-size: 22px;
+          font-weight: bold;
+          margin-right: 20px;
+
+        }
+
+        .label {
+          width: auto;
+          padding: 0 6px;
+          font-size: 12px;
+          height: 20px;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          background-color: #00000020;
+          color: #000000;
+        }
+
+      }
+
+      .bottom {
+        display: flex;
+
+        .item {
+          margin-right: 10px;
+          display: flex;
+
+          .icon {
+            width: 15px;
+            height: 15px;
+            background-color: #00000020;
+            margin-right: 5px;
+          }
+
+          .text {
+            font-size: 12px;
+            color: #00000060;
+          }
+        }
+      }
+    }
+  }
+
+  .navBar {
+    margin-top: 20px;
+    display: flex;
+    position: relative;
+    top: 2px;
+
+    .navItem {
+      color: #00000060;
+      width: 80px;
+      height: 40px;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      margin-right: 40px;
+      cursor: pointer;
+
+      &.select {
+        color: #000000;
+        border-bottom: 4px solid #000000;
+      }
+    }
+
+  }
+}
+
+.contentBox {
+
+  .title {
+    font-size: 22px;
+    position: relative;
+    text-align: left;
+
+    .titleMask {
+      width: 90px;
+      height: 20px;
+      background-color: #00000020;
+      position: absolute;
+      bottom: -5px;
+      left: 20px;
+    }
+  }
+
+  .messageDetails {
+    box-sizing: border-box;
+    width: 100%;
+    padding: 40px 5px 0 5px;
+    display: flex;
+    flex-wrap: wrap;
+    row-gap: 15px;
+
+    .messageItem {
+      box-sizing: border-box;
+      flex: 0 0 50%;
+      text-align: left;
+      display: flex;
+      //justify-content: center;
+      align-items: center;
+
+      .doll {
+        width: 5px;
+        height: 5px;
+        background-color: #000000;
+        margin-right: 10px;
+      }
+
+
+    }
+  }
+
+  .educationDetail {
+    margin-top: 40px;
+    text-align: left;
+
+    .schoolName {
+
+      margin-bottom: 20px;
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      .name{
+        font-weight: bold;
+      }
+      .time{
+        font-size: 14px;
+      }
+    }
+  .educationItem{
+    display: flex;
+    flex-direction: column;
+    row-gap: 15px;
+  }
+  }
+}
+
+</style>
